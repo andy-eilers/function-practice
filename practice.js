a1b1f91681c1a7ac9ed502f4f9573d64e8719608
andy.eilers <andy.eilers@asurion.com>
2021-11-16 09:18:42 -0600

    first commit

---
+++ practice.js
@@ -0,0 +1,24 @@
+/*
+const calculateAgeInDogYears = (ageOfPerson) => {
+    const ageInDogYears = ageOfPerson / 7
+    
+    return ageInDogYears
+}
+
+const dogAge = calculateAgeInDogYears(45)
+console.log(dogAge)
+*/
+
+const isMyFavoriteDogBreed = (dogBreed) => {
+
+    return `My favorite dog breed is ${dogBreed}` 
+
+}
+
+const myFavorite = isMyFavoriteDogBreed(Corgi)
+
+if (dogBreed === "Meow") {
+    console.log("I like cats")
+} else {
+    console.log(`When it comes to pets, ${myFavorite}`)
+}
